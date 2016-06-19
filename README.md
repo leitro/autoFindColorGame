e93a275614bd4837d4dc1068a23ed2c78748356e
kanglei <kanglei2004@sjtu.edu.cn>
2016-06-19 18:18:39 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # autoFindColorGame
+Play the Find-Color-Game automatically.
+
+###Requirements:
+- OpenCV 3.0
+- Python 3
+- pyscreenshot
+- xdotool
+
+###How to use:
+1. Open the [Find-Color-Game](http://www.4399.com/flash/144484_2.htm) link address.
+2. Use "python3 findColor.py" to start the program.
+3. A screenshot will appear on your screen, and you need to click the top-left corner and then the bottom-right corner of the target game area.
+4. Press the space button and it will work.
+
+###Watch my DEMO video:
+[http://www.bilibili.com/video/av5023951/](http://www.bilibili.com/video/av5023951/)
---
+++ findColor.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+import os
+import pyscreenshot as ss
+import time
+
+def onMouse(event, x, y, flags, param):
+    global flag, pLeftTop, pRightBottom
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if flag:
+            pLeftTop = (x,y)
+        else:
+            pRightBottom = (x,y)
+        flag = False
+
+flag = True
+pLeftTop = None
+pRightBottom = None
+
+cv2.namedWindow('frame')
+cv2.setMouseCallback('frame', onMouse)
+
+img = ss.grab(bbox=(0,0,1300,768))
+img = np.array(img)
+cv2.imshow('frame', img)
+if cv2.waitKey(0) & 0xff == ord(' '):
+    cv2.destroyWindow('frame')
+#print(pLeftTop, pRightBottom)
+while True:
+    img = ss.grab(bbox=(0,0,1300,768))
+    img = np.array(img)
+    target = img[pLeftTop[1]:pRightBottom[1],pLeftTop[0]:pRightBottom[0]]
+    gray = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY_INV)
+    im, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    centers = []
+    centersGray = []
+    for c in contours:
+        (x,y), radius = cv2.minEnclosingCircle(c)
+        x, y = int(x), int(y)
+        centers.append((x,y))
+        centersGray.append(gray[y,x])
+    sets = set(centersGray)
+    index = None
+    #print(centers,sets)
+    for s in sets:
+        if centersGray.count(s) == 1:
+            index = centersGray.index(s)
+            break
+    realX = centers[index][0] + pLeftTop[0]
+    realY = centers[index][1] + pLeftTop[1]
+    os.system('xdotool mousemove '+str(realX)+' '+str(realY))
+    os.system('xdotool click 1')
+    print(realX,realY)
